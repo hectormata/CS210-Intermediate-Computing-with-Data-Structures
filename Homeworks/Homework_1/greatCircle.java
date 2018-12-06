9c34a1f76c28ded3325372180c41eb84e7224c6b
hectormata <hector.mata9@gmail.com>
2018-12-06 14:27:59 -0500

    Update greatCircle.java

--- Homeworks/Homework_1/greatCircle.java
+++ Homeworks/Homework_1/greatCircle.java
@@ -22,38 +22,12 @@ public class GreatCircle {
         double x2 = Math.toRadians(Double.parseDouble(args[2]));
         double y2 = Math.toRadians(Double.parseDouble(args[3]));
 
-       /*************************************************************************
-        * Compute using law of cosines
-        *************************************************************************/
-        // great circle distance in radians
-        double angle1 = Math.acos(Math.sin(x1) * Math.sin(x2)
-                      + Math.cos(x1) * Math.cos(x2) * Math.cos(y1 - y2));
+        double d = 111 * Math.acos(Math.sin(x1) * Math.sin(x2)
+               + Math.cos(x1) * Math.cos(x2) * Math.cos(y1 - y2));
 
-        // convert back to degrees
-        angle1 = Math.toDegrees(angle1);
+        double D = Math.toDegrees(d);
 
-        // each degree on a great circle of Earth is 60 nautical miles
-        double distance1 = 60 * angle1;
-
-        System.out.println(distance1 + " nautical miles");
-
-
-       /*************************************************************************
-        * Compute using Haverside formula
-        *************************************************************************/
-        double a = Math.pow(Math.sin((x2-x1)/2), 2)
-                 + Math.cos(x1) * Math.cos(x2) * Math.pow(Math.sin((y2-y1)/2), 2);
-
-        // great circle distance in radians
-        double angle2 = 2 * Math.asin(Math.min(1, Math.sqrt(a)));
-
-        // convert back to degrees
-        angle2 = Math.toDegrees(angle2);
-
-        // each degree on a great circle of Earth is 60 nautical miles
-        double distance2 = 60 * angle2;
-
-        System.out.println(distance2 + " nautical miles");
+        System.out.println(D);
     }
 
 }
